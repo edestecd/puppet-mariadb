5cd5b076f76816a4911af6c21765f56d250ba891
Chris Edester <edestecd@miamioh.edu>
2016-09-07 13:24:35 -0400

    Different apt key for newer ubuntu / debian

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -7,13 +7,21 @@ class mariadb::repo::apt {
 
   $version = $mariadb::repo::repo_version
   $os      = $mariadb::repo::os
+  if (($::operatingsystem == 'Debian') and (versioncmp($::operatingsystemrelease, '8.0') >= 0)) or
+  (($::operatingsystem == 'Ubuntu') and (versioncmp($::operatingsystemrelease, '16.04') >= 0)) {
+    $key = {
+      'id' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+    }
+  } else {
+    $key = {
+      'id' => '199369E5404BD5FC7D2FE43BCBCB082A1BB943DB',
+    }
+  }
 
   apt::source { 'mariadb':
     location => "http://nyc2.mirrors.digitalocean.com/mariadb/repo/${version}/${os}",
     repos    => 'main',
-    key      => {
-      'id' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-    },
+    key      => $key,
   }
   # lint:ignore:spaceship_operator_without_tag
   Apt::Source['mariadb'] -> Class['apt::update'] -> Package<| |>
