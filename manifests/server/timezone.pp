b7d5fb57a12d8477cf4f59d4b3dd5a1155fe3b17
Chris Edester <edestecd@miamioh.edu>
2016-09-08 13:32:05 -0400

    Don't chop the timezone sql

--- manifests/server/timezone.pp
+++ manifests/server/timezone.pp
@@ -16,14 +16,14 @@ class mariadb::server::timezone {
   $tzinfo_command   = 'mysql_tzinfo_to_sql /usr/share/zoneinfo'
   # replicate_myisam is failing here... so disable it and do it on all nodes instead
   # This feature is experimental anyways and not recommended on production, so why set it GLOBALLY ???
-  $no_wsrep_command = "sed 's/SET GLOBAL wsrep_replicate_myisam.*;/SET SESSION wsrep_on=OFF;/'" # SET SESSION wsrep_on=OFF;
+  # $no_wsrep_command = "sed 's/SET GLOBAL wsrep_replicate_myisam.*;/SET SESSION wsrep_on=OFF;/'" # SET SESSION wsrep_on=OFF;
 
   $mysql_command = join(['mysql', "--defaults-extra-file=${::root_home}/.my.cnf"], ' ')
   $mysql_execute = join([$mysql_command, '-NBe'], ' ')
 
   # Run sql to set up time zones
   exec { 'mysql_load_timezone_support':
-    command   => "${tzinfo_command} | ${no_wsrep_command} | ${mysql_command} mysql",
+    command   => "${tzinfo_command} | ${mysql_command} mysql",
     cwd       => $mysql::params::datadir,
     onlyif    => "${mysql_execute} 'SELECT COUNT(*) = 0 FROM mysql.time_zone_name;' | grep -q 1",
     logoutput => on_failure,
