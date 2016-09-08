3ef39e105163dc7bebe387712bbeb89cdd2a533f
Chris Edester <edestecd@miamioh.edu>
2016-09-08 13:39:25 -0400

    Check for defaults_extra_file in facts

--- lib/facter/wsrep_status.rb
+++ lib/facter/wsrep_status.rb
@@ -12,14 +12,18 @@
 # Notes:
 #   none
 
+def defaults_extra_file
+  "#{Facter.value(:root_home)}/.my.cnf"
+end
+
 def mysql_execute(sql)
-  mysql_command = ['mysql', "--defaults-extra-file=#{Facter.value(:root_home)}/.my.cnf", '-NBe']
+  mysql_command = ['mysql', "--defaults-extra-file=#{defaults_extra_file}", '-NBe']
   %(#{mysql_command.join(' ')} "#{sql}")
 end
 
 Facter.add(:wsrep_status) do
   setcode do
-    if Facter::Util::Resolution.which('mysql')
+    if Facter::Util::Resolution.which('mysql') && File.file?(defaults_extra_file)
       query = Facter::Util::Resolution.exec(mysql_execute("SHOW STATUS LIKE 'wsrep%';"))
       Hash[query.split("\n").map { |t| t.split("\t") }] unless !query || query.empty?
     end
