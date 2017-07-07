e440f9b30c381b205f221134c00271675398d913
Johannes Hölzel <johannes.hoelzel@we-conect.com>
2017-07-07 17:51:18 +0200

    formated and nicefied ;)

---
+++ examples/simple_cluster.pp
@@ -0,0 +1,27 @@
+# Setup a Mariadbcluster with 3 Nodes
+
+node 'local.localdomain' {
+    class {
+        'apt':
+    }
+    class {
+        'mariadb::cluster': wsrep_cluster_peers => delete(['192.168.1.1', '192.168.1.2', '192.168.1.3'], $::ipaddress),
+        wsrep_cluster_name => 'my_super_cluster',
+        wsrep_sst_password => 'super_secret_password',
+        wsrep_sst_method => 'xtrabackup-v2',
+        root_password => 'another_secret_password',
+        override_options => {
+            'mysqld' => {
+                'performance_schema' => undef,
+                'innodb_file_per_table' => 'OFF',
+            },
+        },
+        galera_override_options => {
+            'mysqld' => {
+                'wsrep_slave_threads' => '2',
+                'innodb_flush_log_at_trx_commit' => '0',
+            },
+        }
+    }
+
+}
