ebf2fa3733dc1c12f2913c8fc3c65ec40a4a6bca
JHoelzel <johannes@hoelzel.it>
2017-07-16 15:01:15 +0200

    fixing indentation warning

--- examples/simpleCluster/Node1.pp
+++ examples/simpleCluster/Node1.pp
@@ -1,54 +1,53 @@
 #Setup a Mariadbcluster with 3 Nodes
-
 node 'localcluster1.localdomain' {
   class {
     'apt':
   }
   class { 'mariadb::cluster' :
-      wsrep_cluster_peers     => delete(['192.168.1.1', '192.168.1.2', '192.168.1.3'], $::ipaddress),
-      wsrep_cluster_name      => 'my_super_cluster',
-      wsrep_sst_password      => 'super_secret_password',
-      wsrep_sst_method        => 'xtrabackup-v2',
-      root_password           => 'another_secret_password',
-      override_options        => {
-        'mysqld' => {
-          'performance_schema'    => undef,
-          'innodb_file_per_table' => 'OFF',
-          },
-      },
-      galera_override_options => {
+    wsrep_cluster_peers     => delete(['192.168.1.1', '192.168.1.2', '192.168.1.3'], $::ipaddress),
+    wsrep_cluster_name      => 'my_super_cluster',
+    wsrep_sst_password      => 'super_secret_password',
+    wsrep_sst_method        => 'xtrabackup-v2',
+    root_password           => 'another_secret_password',
+    override_options        => {
       'mysqld' => {
-          'wsrep_slave_threads'            => '2',
-          'innodb_flush_log_at_trx_commit' => '0',
-          },
-        }
+        'performance_schema'    => undef,
+        'innodb_file_per_table' => 'OFF',
+        },
+      },
+    galera_override_options => {
+      'mysqld' => {
+        'wsrep_slave_threads'            => '2',
+        'innodb_flush_log_at_trx_commit' => '0',
+        },
+      }
     }
-    mariadb::utility::database { 'production dbname' :
-      ensure  => 'present',
-      host    => 'localhost',
-      dbname  => 'dbname',
-      charset => 'utf8',
-      collate => 'utf8_general_ci'
-    }
-    mariadb::user { 'foo' :
-      password   => 'pw',
-      privileges => ['all'],
-      host       => 'localhost',
-    }
-    mariadb::utility::db_grant { 'allow foo acces to production dbname' :
-      ensure => 'present',
-      dbname => 'dbname',
-      host   => 'localhost',
-      grant  => 'ALL',
-      user   => 'foo',
-      table  => '*',
-    }
-    mariadb::utility::db_grant { 'allow foo remote acces to production dbname' :
-      ensure => 'present',
-      dbname => 'dbname',
-      host   => '%',
-      grant  => 'ALL',
-      user   => 'foo',
-      table  => '*',
-    }
+  mariadb::utility::database { 'production dbname' :
+    ensure  => 'present',
+    host    => 'localhost',
+    dbname  => 'dbname',
+    charset => 'utf8',
+    collate => 'utf8_general_ci'
   }
+  mariadb::user { 'foo' :
+    password   => 'pw',
+    privileges => ['all'],
+    host       => 'localhost',
+  }
+  mariadb::utility::db_grant { 'allow foo acces to production dbname' :
+    ensure => 'present',
+    dbname => 'dbname',
+    host   => 'localhost',
+    grant  => 'ALL',
+    user   => 'foo',
+    table  => '*',
+  }
+  mariadb::utility::db_grant { 'allow foo remote acces to production dbname' :
+    ensure => 'present',
+    dbname => 'dbname',
+    host   => '%',
+    grant  => 'ALL',
+    user   => 'foo',
+    table  => '*',
+  }
+}
--- examples/simpleCluster/Node3.pp
+++ examples/simpleCluster/Node3.pp
@@ -5,22 +5,22 @@ node 'localcluster3.localdomain' {
     'apt':
   }
   class { 'mariadb::cluster' :
-      wsrep_cluster_peers     => delete(['192.168.1.1', '192.168.1.2', '192.168.1.3'], $::ipaddress),
-      wsrep_cluster_name      => 'my_super_cluster',
-      wsrep_sst_password      => 'super_secret_password',
-      wsrep_sst_method        => 'xtrabackup-v2',
-      root_password           => 'another_secret_password',
-      override_options        => {
-        'mysqld' => {
-          'performance_schema'    => undef,
-          'innodb_file_per_table' => 'OFF',
-          },
-      },
-      galera_override_options => {
+    wsrep_cluster_peers     => delete(['192.168.1.1', '192.168.1.2', '192.168.1.3'], $::ipaddress),
+    wsrep_cluster_name      => 'my_super_cluster',
+    wsrep_sst_password      => 'super_secret_password',
+    wsrep_sst_method        => 'xtrabackup-v2',
+    root_password           => 'another_secret_password',
+    override_options        => {
       'mysqld' => {
-          'wsrep_slave_threads'            => '2',
-          'innodb_flush_log_at_trx_commit' => '0',
-          },
-        }
+        'performance_schema'    => undef,
+        'innodb_file_per_table' => 'OFF',
+        },
+      },
+    galera_override_options => {
+      'mysqld' => {
+        'wsrep_slave_threads'            => '2',
+        'innodb_flush_log_at_trx_commit' => '0',
+        },
+      }
     }
   }
