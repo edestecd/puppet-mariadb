b6c2d7d43041d72f3df6e0ed94c6d62d4bdeda36
JHoelzel <johannes@hoelzel.it>
2017-07-16 14:45:06 +0200

    Manage the percona apt repo. with different keys for ubuntu and debian

--- manifests/repo/percona/apt.pp
+++ manifests/repo/percona/apt.pp
@@ -1,16 +1,26 @@
 # apt.pp
 # Manage the percona apt repo.
+# with different keys for ubuntu and debian
 #
 
 class mariadb::repo::percona::apt {
-  include ::apt
+    include ::apt
 
-  apt::source { 'percona-release':
-    location => 'http://repo.percona.com/apt',
-    repos    => 'main',
-    key      => {
-      'id' => '430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A',
-    },
-  }
-  Apt::Source['percona-release'] -> Class['apt::update'] -> Package<| tag == 'percona' |>
+    if ($::operatingsystem == 'Debian' and versioncmp($::operatingsystemrelease, '8.0') >= 0)
+    or ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '16.04') >= 0) {
+        $key = {
+            'id' => '4D1BB29D63D98E422B2113B19334A25F8507EFA5',
+        }
+    } else {
+        $key = {
+            'id' => '430BDF5C56E7C94E848EE60C1C4CBDCDCD2EFD2A',
+        }
+    }
+
+    apt::source {
+        'percona-release': location => 'http://repo.percona.com/apt',
+        repos    => 'main',
+        key      => $key,
+    }
+    Apt::Source['percona-release'] -> Class['apt::update'] -> Package<| tag == 'percona' |>
 }
